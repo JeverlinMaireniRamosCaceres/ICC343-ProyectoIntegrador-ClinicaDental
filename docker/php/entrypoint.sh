5fb7106e3584ae7685b52c8b311f06ce6ce05350
Emil Paulino <efpl0001@ce.pucmm.edu.do>
2026-06-29 21:45:15 -0400

    .

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+echo "→ Sincronizando public..."
+cp -r /var/www/html/public-source/. /var/www/html/public/
+
 echo "→ Optimizando configuración..."
 php artisan config:cache
 php artisan route:cache
